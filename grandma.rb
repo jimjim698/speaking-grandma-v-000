9b14e4d77b5d8bc5147a8b93fd4f319e656e855f
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-12 04:06:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(statement)
-  if statement == "I LOVE YOU GRANDMA!" 
+  if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif statement == statement.upcase
     return "NO, NOT SINCE 1938!"
