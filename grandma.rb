0df1b29b18efa2e3d2ac83e55b483227a400e42b
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-12 04:01:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(statement)
   elsif statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
-      puts "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
     end
   end
